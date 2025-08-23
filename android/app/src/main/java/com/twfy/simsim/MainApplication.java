33559cc0014d310b43fc897b3b623717b395e979
taxiiraq <user@example.com>
2025-08-23 23:28:52 +0300

    رسالة التعديلات

--- android/app/src/main/java/com/twfy/simsim/MainApplication.java
+++ android/app/src/main/java/com/twfy/simsim/MainApplication.java
@@ -26,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
       }
 
       @Override
-      protected List<ReactPackage> getReactPackages() {
+      protected List<ReactPackage> getPackages() {
         List<ReactPackage> packages = new PackageList(this).getPackages();
         return packages;
       }
