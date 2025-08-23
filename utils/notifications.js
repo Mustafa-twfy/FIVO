63b6953ca3b6a67b7de4db34b0a2309fb9bbd7f0
taxiiraq <user@example.com>
2025-08-23 18:09:26 +0300

    رسالة التعديلات

--- utils/notifications.js
+++ utils/notifications.js
@@ -1,5 +1,4 @@
 import * as Notifications from 'expo-notifications';
-import * as Device from 'expo-device';
 import { Platform } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { supabase } from '../supabase';
@@ -38,7 +37,7 @@ class NotificationService {
       }
 
       // الحصول على Expo Push Token
-      if (Device.isDevice) {
+      if (Platform.OS !== 'web') {
         this.expoPushToken = await Notifications.getExpoPushTokenAsync({
           projectId: '7afda903-8cd5-422f-9192-e535509be738', // من app.json
         });
