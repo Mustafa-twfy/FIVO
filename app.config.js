6ac8bbfce8557d3657d505f0480e793bcdaf7fad
taxiiraq <user@example.com>
2025-08-24 14:24:07 +0300

    رسالة التعديلات

---
+++ app.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  expo: {
+    name: "سمسم",
+    slug: "simsim",
+    version: "1.0.0",
+    orientation: "portrait",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    icon: "./assets/icon.png",
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#00C897"
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.twfy.simsim"
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#00C897"
+      },
+      package: "com.twfy.simsim",
+      googleServicesFile: "./google-services.json"
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    plugins: [
+      [
+        "expo-notifications",
+        {
+          icon: "./assets/notification-icon.png",
+          color: "#00C897"
+        }
+      ]
+    ],
+    extra: {
+      eas: {
+        projectId: "7afda903-8cd5-422f-9192-e535509be738"
+      }
+    }
+  }
+};
