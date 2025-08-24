8edc7cd1c532cf2d207e0f63eefb8c6a4b32523b
taxiiraq <user@example.com>
2025-08-24 05:21:58 +0300

    رسالة التعديلات

--- android/app/src/main/java/com/twfy/simsim/MainActivity.java
+++ android/app/src/main/java/com/twfy/simsim/MainActivity.java
@@ -1,22 +1,14 @@
 package com.twfy.simsim;
 
-import android.os.Build;
 import android.os.Bundle;
-
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
-import expo.modules.ReactActivityDelegateWrapper;
-
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    // Set the theme to AppTheme BEFORE onCreate to support
-    // coloring the background, status bar, and navigation bar.
-    // This is required for expo-splash-screen.
-    setTheme(R.style.AppTheme);
     super.onCreate(null);
   }
 
@@ -36,30 +28,10 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, new DefaultReactActivityDelegate(
+    return new DefaultReactActivityDelegate(
         this,
         getMainComponentName(),
         DefaultNewArchitectureEntryPoint.getFabricEnabled()
-    ));
-  }
-
-  /**
-   * Align the back button behavior with Android S
-   * where moving root activities to background instead of finishing activities.
-   * @see <a href="https://developer.android.com/reference/android/app/Activity#onBackPressed()">onBackPressed</a>
-   */
-  @Override
-  public void invokeDefaultOnBackPressed() {
-    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
-      if (!moveTaskToBack(false)) {
-        // For non-root activities, use the default implementation to finish them.
-        super.invokeDefaultOnBackPressed();
-      }
-      return;
-    }
-
-    // Use the default back button implementation on Android S
-    // because it's doing more than [Activity.moveTaskToBack] in fact.
-    super.invokeDefaultOnBackPressed();
+    );
   }
 }
